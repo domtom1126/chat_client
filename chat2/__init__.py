8ffd66e9abf7dd0604665eb3672fe46f5a5e53a4
Dominic <dom@Dominics-MacBook-Air.local>
2021-04-19 23:05:20 -0400

    Messages post. and show up on seperate window

--- chat2/__init__.py
+++ chat2/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_socketio import SocketIO, send
 import json
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -7,18 +8,31 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    socketio = SocketIO(app)
 
-    with open('/etc/config.json') as config_file:
-        config = json.load(config_file)
+    @socketio.on('message')
+    def handleMessage(msg):
+        print('Message: ' + msg)
+        send(msg, broadcast=True)
 
-    app.config['SECRET_KEY'] = config.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
+# SECTION This is for production
+#     # with open('/etc/config.json') as config_file:
+#     #     config = json.load(config_file)
+
+#     # app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+#     # app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
+# !SECTION End of production stuff
+
+    app.config['SECRET_KEY'] = '1234'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+
+    
 
     from .models import User
 
--- chat2/main.py
+++ chat2/main.py
@@ -1,14 +1,23 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from flask_socketio import SocketIO, send
 from . import db
 
 main = Blueprint('main', __name__)
+
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
+
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.username)
+
+
+# @socketio.on('message')
+# def handleMessage(msg):
+#     print('Message:' + msg)
+#     send(msg, broadcast=True)
